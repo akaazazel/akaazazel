6b5c9878f696ee01394571f35270edb598f098cc
akaazazel <117853933+akaazazel@users.noreply.github.com>
2024-03-23 02:08:59 +0530

    Delete javascript directory

--- javascript/google.js
+++
@@ -1,7 +0,0 @@
-function emailSave(){
-  let email = document.querySelector('.email-input').value;
-  localStorage.setItem("email", email);
-}
-
-let email = localStorage.getItem("email");
-document.querySelector('.email-preview').innerHTML = email;
